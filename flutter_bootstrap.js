efec8c71194c236cdab2ebdd16ede887011eb9d9
Waleed Ahmad <24100300@lums.edu.pk>
2024-06-09 22:27:31 +0500

    Images compression

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "570673219"
+    serviceWorkerVersion: "366498595"
   }
 });
