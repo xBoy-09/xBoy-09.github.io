eb3ffa48d0a34c4b455ef0d38b0fa6516d6ec467
Waleed Ahmad <24100300@lums.edu.pk>
2024-06-23 21:57:59 +0500

    Service section done

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1032500181"
+    serviceWorkerVersion: "2830680719"
   }
 });
